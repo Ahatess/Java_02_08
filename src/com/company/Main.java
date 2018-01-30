4dbd3c6960c2283a6a007304b2367fa75d6eb131
Andrius <andriuscere@gmail.com>
2018-01-30 16:00:26 +0200

    Uzduotis8

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Irasykite nuvaziuotus KM");
+	float a = sc.nextFloat();
+	System.out.println("Irasykite kuro sanaudas L");
+	float b = sc.nextFloat();
+	System.out.println("Jusu vidutines kuro sanaudos: " +((b*100)/a) );
+
     }
 }
